189aa151e09efbb27b267a6514e587cf80c718cf
郑永帅 <zhengyongshuai@digibird.com.cn>
2023-05-19 23:40:10 +0800

    创建项目

---
+++ src/main/java/edu/healthpasskeeper/HealthPassKeeperApplication.java
@@ -0,0 +1,13 @@
+package edu.healthpasskeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPassKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthPassKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/healthpasskeeper/HealthPassKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.healthpasskeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthPassKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
